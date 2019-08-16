bcb125a0eaf415195526186d681af51d53e75892
lozadstv <54184004+lozadstv@users.noreply.github.com>
2019-08-16 20:08:08 +0200

    includes

---
+++ includes/footer.php
@@ -0,0 +1,88 @@
+<div id="footer"><!-- #footer Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="row"><!-- row Begin -->
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+               
+               <h4>General Manager:</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="#">Mesfin Hailegiorgis</a></li>
+                    
+                </ul><!-- ul Finish -->
+                
+                <hr>
+                
+                
+                
+                <hr class="hidden-md hidden-lg hidden-sm">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Vice Manager:</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="#">Yibo</a></li>
+                    <li><a href="#">Wul</a></li>
+                    
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Find Us</h4>
+                
+                <p><!-- p Start -->
+                    
+                    <strong>LozaDStv</strong>
+                    <br/>Hawassa
+                    <br/>Addis Ketema k/k
+                    <br/>Infront of Selam Grocery
+                    <br/>+251 916106528
+                    <br/>eliashgiorgis@gmail.com
+                    
+                    
+                </p><!-- p Finish -->
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3">
+                
+                <hr>
+                
+                <h4>Keep In Touch</h4>
+                
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+                
+            </div>
+        </div><!-- row Finish -->
+    </div><!-- container Finish -->
+</div><!-- #footer Finish -->
+
+
+<div id="copyright"><!-- #copyright Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-left">&copy; 2019 LozaDStv All Rights Reserve</p>
+            
+        </div><!-- col-md-6 Finish -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-right">Theme by: <a href="#">Elias Hailegiorgis</a></p>
+            
+        </div><!-- col-md-6 Finish -->
+    </div><!-- container Finish -->
+</div><!-- #copyright Finish -->
